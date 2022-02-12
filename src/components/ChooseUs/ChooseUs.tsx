ff7fcfb3d5a78d1891b24abfe642827d349a5119
Prince Narteh <prinarteh@gmail.com>
2022-02-12 15:17:36 +0000

    adds icons to ChooseUs InfoBox

--- src/components/ChooseUs/ChooseUs.tsx
+++ src/components/ChooseUs/ChooseUs.tsx
@@ -7,24 +7,32 @@ import { Paragraph } from "../styles/Paragraph";
 import { Section } from "../styles/Section";
 import { Title } from "../styles/Title";
 import { Wrapper } from "../styles/Wrapper";
+import { BiCheckShield } from "react-icons/bi";
+import { WiStars } from "react-icons/wi";
+import { RiCustomerService2Fill } from "react-icons/ri";
+import { GiTrophyCup } from "react-icons/gi";
 
 const advantages = [
   {
+    icon: <BiCheckShield size={30} color="#2cd97b" />,
     title: "Protection",
     description:
       "Vivamus posuere suscipit nulla maecenas et dapibus eros diam nec consequat aptent.",
   },
   {
+    icon: <WiStars size={30} color="#2cd97b" />,
     title: "Good Review",
     description:
       "Vivamus posuere suscipit nulla maecenas et dapibus eros diam nec consequat aptent.",
   },
   {
+    icon: <RiCustomerService2Fill size={30} color="#2cd97b" />,
     title: "24/7 Support",
     description:
       "Vivamus posuere suscipit nulla maecenas et dapibus eros diam nec consequat aptent.",
   },
   {
+    icon: <GiTrophyCup size={30} color="#2cd97b" />,
     title: "Prestigious",
     description:
       "Vivamus posuere suscipit nulla maecenas et dapibus eros diam nec consequat aptent.",
@@ -34,7 +42,7 @@ const advantages = [
 export const ChooseUs = () => {
   return (
     <ChooseUsContainer>
-      <Column width={50}>
+      <Column width={40}>
         <Heading>Why Choose Us</Heading>
         <Title as="h3">
           See the difference professional services can do for you
@@ -44,10 +52,11 @@ export const ChooseUs = () => {
           tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
         </Paragraph>
       </Column>
-      <Column>
+      <Column width={60}>
         <Wrapper>
           {advantages.map((advantage, idx) => (
             <Card key={idx}>
+              <div className="icon">{advantage.icon}</div>
               <Title as="h6" fontSize={2.5}>
                 {advantage.title}
               </Title>
@@ -66,7 +75,7 @@ const ChooseUsContainer = styled(Section)`
 
 const Card = styled(InfoBox)`
   flex: 1;
-  min-width: 25rem;
+  min-width: 22rem;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
@@ -78,5 +87,9 @@ const Card = styled(InfoBox)`
     font-size: 1.5rem;
     width: 90%;
     line-height: 1.5;
+  }
+
+  .icon {
+    margin-bottom: 1rem;
   }
 `;
